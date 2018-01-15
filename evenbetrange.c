45c9c869c85f05fb3c21c5fecc10bbf37f13e71b
kshitijbansal <34815496+kshitijbansal@users.noreply.github.com>
2018-01-15 22:31:32 +0530

    Create evenbetrange.c

---
+++ evenbetrange.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+   int x;
+   int y;
+   int a[100000];
+ scanf("%d",&x);
+ scanf("%d",&y);
+ for(int i=x+1;i<y;i++)
+ {
+     if(i%2==0)
+     {
+
+             printf("%d ",i);
+         
+     }
+    
+     
+ }
+
+}
