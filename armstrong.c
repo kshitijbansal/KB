6541bffe076f23d1bcf3ef7c1a273ec97954fbe6
kshitijbansal <34815496+kshitijbansal@users.noreply.github.com>
2018-01-16 19:10:36 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+   int x,y,a;
+   int s=0;
+ scanf("%d",&x);
+ y=x;
+ while(x!=0)
+ {
+     a=x%10;
+     s=a*a*a+s;
+     x=x/10;
+ }
+ if(s==y)
+ printf("armstrong");
+ else
+ printf("no");
+}
+ 
